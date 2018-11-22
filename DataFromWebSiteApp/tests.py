0bb68f15293bae091ad3f2387f69e05c1bf52821
JacekEjsmont <jacek.ejsmont@gmail.com>
2018-11-22 19:18:04 +0100

    Revert "Adding tests"
    This reverts commit 42270f48c9243f3763f29da970bac2058eb0b267.

--- DataFromWebSiteApp/tests.py
+++ DataFromWebSiteApp/tests.py
@@ -1,8 +1,6 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from .test_data import POST_TEXT_DATA, POST_IMAGES_DATA
-import json
 
 
 class TextFromWebsiteTests(APITestCase):
@@ -18,12 +16,6 @@ class TextFromWebsiteTests(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_content_for_post_text(self):
-        url = reverse('data_from_web_site:scrape_text')
-        data = {'website_url': 'https://www.djangoproject.com/start/overview/'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.content.decode(), json.dumps(POST_TEXT_DATA, separators=(',', ':'), ensure_ascii=False))
-
 
 class ImagesFromWebsiteTests(APITestCase):
     def test_get_stored_images(self):
@@ -37,9 +29,3 @@ class ImagesFromWebsiteTests(APITestCase):
         data = {'website_url': 'https://pl.wikipedia.org/wiki/Komputer'}
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_content_for_post_images(self):
-        url = reverse('data_from_web_site:scrape_images')
-        data = {'website_url': 'https://pl.wikipedia.org/wiki/Matematyka'}
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.content.decode(), json.dumps(POST_IMAGES_DATA, separators=(',', ':'), ensure_ascii=False))
