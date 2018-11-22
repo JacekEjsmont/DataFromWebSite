16fbe1ee529c2a8723102e6e3b2cfff9a0cfd3b4
JacekEjsmont <jacek.ejsmont@gmail.com>
2018-11-22 14:59:41 +0100

    Adding docker

--- DataFromWebSite/settings.py
+++ DataFromWebSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'aw(if7$#0%7#(0b!hwes_@1%n(=v)^=#%mhq%dj(dtqa#8r&kl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
